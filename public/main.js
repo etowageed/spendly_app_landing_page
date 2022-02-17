2f8a5525e29430e72208930c58f361c316336487
Gideon Etowa <etowagideon@gmailcom>
2022-02-17 23:18:46 +0100

    optimized the imgs

--- public/main.js
+++ public/main.js
@@ -5,12 +5,12 @@ gsap.from(".hero-copy", { duration: 2, x: "-100%", ease: "back.out" });
 gsap.from(".hero-image", {
   duration: 2,
   delay: 0.7,
-  x: "105%",
+  x: "100%",
   ease: "back.out",
 });
 
 gsap.from(".sum-copy", { scrollTrigger: {
     trigger:".sum-copy",
     toggleActions: "restart pause resume pause",
-}, duration: 6, x: "100%", ease: "back.out" });
+}, duration: 6, x: "-40%", ease: "back.out" });
 gsap.from(".3d-2", { scrollTrigger: ".3d-2", duration: 3, y: "100%", ease: "back.out" });
